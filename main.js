005476b170ef4167c1631bd39a32c9ebf1624441
droshev <mladen.droshev@sap.com>
2022-04-10 08:25:56 -0400

    feat: initial commit with sample

---
+++ main.js
@@ -0,0 +1,6 @@
+import './style.css'
+
+document.querySelector('#app').innerHTML = `
+  <h1 class="fd-title fd-title--h1">Hello World!</h1>
+  <a class="fd-link" href="https://fundamental-styles.netlify.app" target="_blank">Documentation</a>
+`
